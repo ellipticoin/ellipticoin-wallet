7ac2c445bab9bd110709d9d36b85d5045a143f10
Mason Fischer <mason@kissr.co>
2020-12-01 12:44:10 -0500

    Fix amount input bug in Safari

--- src/Inputs/helpers.js
+++ src/Inputs/helpers.js
@@ -19,7 +19,7 @@ export const formatBigNumAsText = (num, currency) => {
       ? {
           style: "currency",
           currency: "USD",
-          currencyDisplay: "narrowSymbol",
+          currencyDisplay: "symbol",
         }
       : {};
 
